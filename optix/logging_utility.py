58e9d8722b1fec363ca9e4a659bd4d292452106a
Felix Igelbrink <felix.igelbrink@uni-osnabrueck.de>
2021-09-28 14:53:17 +0200

    added docstring

--- optix/logging_utility.py
+++ optix/logging_utility.py
@@ -2,6 +2,25 @@ import logging
 
 
 class Logger(object):
+    """
+    Helper class to translate the optix logging calls to python logging calls.
+    It takes any Logger compatible object and outputs all messages with the following log levels:
+
+    OptiX   | Logging
+    -----------------
+      1     |  FATAL
+      2     |  ERROR
+      3     |  WARN
+      4     |  DEBUG
+
+    See also https://raytracing-docs.nvidia.com/optix7/api/html/group__optix__types.html#gafa74ebb0b1ab57289a4d5a080cd4c090
+    for more information.
+
+    Parameters
+    ----------
+    logger: logging.Logger or compatible object
+        The logger to output the messages to
+    """
     _optix_to_logging_level = {
         4: logging.DEBUG,
         3: logging.WARN,
