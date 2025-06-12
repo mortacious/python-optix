5e9aa242528f2a2f252f032945ff9f0b6f8fda55
Felix Igelbrink <mortacious@googlemail.com>
2025-06-12 13:20:06 +0200

    WIP adapting setup

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from Cython.Build import cythonize
 import re
 import os
 from pathlib import Path
-import shutil
 import numpy
 
-OPTIX_COMPATIBLE_VERSION = (7, 7, 0)
+OPTIX_COMPATIBLE_VERSION = (9, 0, 0)
 
 
 # standalone import of a module (https://stackoverflow.com/a/58423785)
@@ -86,75 +85,17 @@ with util.temp_optix_include_path(OPTIX_COMPATIBLE_VERSION) as optix_include_pat
         return [str(path.relative_to(package_path))
                 for path in package_path.glob(glob)]
 
-
-    # from setuptools.command.install import install as _install
-    # from setuptools.command.develop import develop as _develop
-
-
-    # class EmbeddHeadersCommandMixin:
-    #     def update_package_data(self):
-    #         self.distribution.package_data.update({
-    #             'optix': [*glob_fix('optix', 'include/**/*')]
-    #         })
-    #         print("embedding optix headers into package data",
-    #             self.distribution.package_data)
-
-    #     def run(self):
-    #         embedd = os.getenv("OPTIX_EMBED_HEADERS")
-    #         if embedd:
-    #             # create the path for the internal headers
-    #             # due to optix license restrictions those headers
-    #             # cannot be distributed on pypi directly so we will add this headers dynamically
-    #             # upon wheel construction to install them alongside the package
-
-    #             if not os.path.exists('optix/include/optix.h'):
-    #                 shutil.copytree(optix_include_path, 'optix/include')
-
-    #             self.update_package_data()
-
-    #         super().run()
-
-
-    # class CustomInstallCommand(EmbeddHeadersCommandMixin, _install):
-    #     pass
-
-
-    # class CustomDevelopCommand(EmbeddHeadersCommandMixin, _develop):
-    #     pass
-
-
-    # cmd_classes = {'install': CustomInstallCommand,
-    #             'develop': CustomDevelopCommand}
-
-    # try:
-    #     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-
-    #     class CustomBdistWheelCommand(EmbeddHeadersCommandMixin, _bdist_wheel):
-    #         pass
-    #     cmd_classes['bdist_wheel'] = CustomBdistWheelCommand
-    # except ImportError:
-    #     CustomBdistWheel = None
-
     setup(
-        name="python-optix",
-        author="Felix Igelbrink",
-        author_email="felix.igelbrink@uni-osnabrueck.de",
         description="Python bindings to the OptiX raytracing engine by nvidia",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/mortacious/python-optix",
-        project_urls={
-            "Bug Tracker": "https://github.com/mortacious/python-optix/issues",
-        },
         packages=find_packages(exclude=['tests', 'examples']),
         ext_modules=extensions,
         install_requires=[
             'numpy',
-            'cupy>=11.0'
         ],
         license="MIT",
         classifiers=[
-            "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
@@ -169,8 +110,11 @@ with util.temp_optix_include_path(OPTIX_COMPATIBLE_VERSION) as optix_include_pat
             "Topic :: Software Development",
         ],
         extras_require={
-            'examples': ["pillow", "pyopengl", "pyglfw", "pyimgui"]
+            'cupy': ['cupy'], # regular cupy (e.g. from conda)
+            'cupy-cuda12x': ['cupy-cuda12x'] # pip installable cupy
+            'examples': ["pillow", "pyopengl", "pyglfw", "pyimgui"] # additional deps for the examples
         },
+
         use_scm_version={
             "write_to": "optix/_version.py",
             "version_scheme": "release-branch-semver",
@@ -179,5 +123,4 @@ with util.temp_optix_include_path(OPTIX_COMPATIBLE_VERSION) as optix_include_pat
         python_requires=">=3.9",
         package_data=package_data,
         zip_safe=False,
-        #cmdclass=cmd_classes
     )
